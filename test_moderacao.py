670cd17fbb9448199b24a4e4fc83060017b40031
magdafdamasceno-del <magdafdamasceno@gmail.com>
2026-07-11 16:32:00 -0300

    Update test_moderacao.py

--- test_moderacao.py
+++ test_moderacao.py
@@ -66,7 +66,7 @@ with col2:
                 
                 try:
                     response = client.models.generate_content(
-                        model='gemini-1.5-flash',
+                        model='gemini-2.0-flash',
                         contents=prompt_sistema,
                     )
                     st.markdown(response.text)
